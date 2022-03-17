46be7c32550e5efce87a90b2eacd8c657589855a
Karim <karim.wael@gmail.com>
2022-03-17 15:57:32 +0400

    clients suck

--- src/Services/SocketClusterClient.ts
+++ src/Services/SocketClusterClient.ts
@@ -1,6 +1,5 @@
 import { SocketClientOptions } from "src/Models/Interfaces/Sockets/SocketClientOptions";
 import SockerClusterClient, { AGClientSocket } from "socketcluster-client";
-
 export class SocketClient {
   token: string;
   client: AGClientSocket;
@@ -11,16 +10,34 @@ export class SocketClient {
     if (SocketClient.instance instanceof SocketClient) {
       return SocketClient.instance;
     } else {
+      console.log(args.token);
       this.token = args.token;
       console.log(args.url);
       this.client = SockerClusterClient.create({ host: args.url });
       this.onSocketConnected();
+      this.MessageListener();
       SocketClient.instance = this;
     }
   }
   setAuthToken(token: string) {
     this.authToken = token;
   }
+
+  async MessageListener() {
+    (async () => {
+      // * Attach listener to commands
+      for await (const data of this.client.receiver("command")) {
+        console.log(data);
+      }
+    })();
+    // this.client.subscribe("899d92cd-ae4f-4223-b5ba-b9969741261e");
+    // for await (let data of this.client.channel(
+    //   "899d92cd-ae4f-4223-b5ba-b9969741261e"
+    // )) {
+    //   console.log(data);
+    // }
+  }
+
   async onSocketConnected() {
     for await (const _ of this.client.listener("connect")) {
       this.Login();
